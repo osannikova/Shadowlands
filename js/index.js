904d5dc9858ecdee9767a86d90959f1860306e65
osannikova <h.sannikova@gmail.com>
2019-12-18 15:34:10 +0000

    sample code

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-document.querySelector('submit').addEventListener('click', getData);
+/*document.querySelector('submit').addEventListener('click', getData);
 function getData() {
     fetch('')
     .then(response => response.json())
     .then(json => console.log(json))
 }
-
+*/
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -30,4 +30,42 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); 
+}
+
+
+function login(form) {
+    var un = form.Username.value;
+    var pw = form.Password.value;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("post", "Login", true);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            loginResults();
+        }
+    }
+}
+
+
+window.addEventListener(window,"load", function() {
+    var loginForm = document.getElementById("LoginForm");
+    window.addEventListener(loginForm, "submit", function() {
+         login(loginForm);
+     });
+ });
+
+ function loginResults() {
+    var loggedIn = document.getElementById("LoggedIn");
+    var badLogin = document.getElementById("BadLogin");
+    if (xmlhttp.responseText.indexOf("failed") == -1) {
+        loggedIn.innerHTML = "Logged in as " + xmlhttp.responseText;
+        loggedIn.style.display = "block";
+        form.style.display = "none";
+    } else {
+        badLogin.style.display = "block";
+        form.Username.select();
+        form.Username.className = "Highlighted";
+        setTimeout(function() {
+            badLogin.style.display = 'none';
+        }, 3000);
+    }
 }
