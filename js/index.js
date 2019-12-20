372e572ed867095b8c4ac6f5f0a32b382effc506
osannikova <h.sannikova@gmail.com>
2019-12-20 05:28:17 +0000

    sample code

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function getData() {
 }
 */
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -17,55 +17,18 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function showSlides(n) {
+  
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); 
-}
-
-
-function login(form) {
-    var un = form.Username.value;
-    var pw = form.Password.value;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("post", "Login", true);
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            loginResults();
-        }
-    }
-}
-
-
-window.addEventListener(window,"load", function() {
-    var loginForm = document.getElementById("LoginForm");
-    window.addEventListener(loginForm, "submit", function() {
-         login(loginForm);
-     });
- });
-
- function loginResults() {
-    var loggedIn = document.getElementById("LoggedIn");
-    var badLogin = document.getElementById("BadLogin");
-    if (xmlhttp.responseText.indexOf("failed") == -1) {
-        loggedIn.innerHTML = "Logged in as " + xmlhttp.responseText;
-        loggedIn.style.display = "block";
-        form.style.display = "none";
-    } else {
-        badLogin.style.display = "block";
-        form.Username.select();
-        form.Username.className = "Highlighted";
-        setTimeout(function() {
-            badLogin.style.display = 'none';
-        }, 3000);
-    }
+  dots[slideIndex-1].className += " active";
 }
